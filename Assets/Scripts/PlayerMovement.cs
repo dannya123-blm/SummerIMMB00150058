49b7d6c40905450395dfc0afb388002f84dca69c
dannya123-blm <B00150058@mytudublin.ie>
2024-06-03 13:28:53 +0100

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody rb;
+    private Animator animator;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+        UpdateAnimator();
+    }
+
+    private void Move()
+    {
+        float moveInput = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+        }
+
+        Vector3 move = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0);
+        rb.velocity = move;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        animator.SetBool("IsGrounded", isGrounded);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
